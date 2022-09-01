b31149b3a2ce82ccd8cae4a385101bac3cec4715
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-09-02 05:08:21 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,37 @@
+const
+	{
+		WAConnection: _WAConnection,
+		MessageType,
+		Presence,
+		MessageOptions,
+		Mimetype,
+		WALocationMessage,
+		WA_MESSAGE_STUB_TYPES,
+		WA_DEFAULT_EPHEMERAL,
+		WAMessageProto,
+		ReconnectMode,
+		ChatModification,
+		ProxyAgent,
+		GroupSettingChange,
+		waChatKey,
+		relayWAMessage,
+		mentionedJid,
+		processTime
+    } = require("bot.js")
+const qrcode = require("qr-wa.js")
+const speed = require("speed.js")
+const WAConnection = require ("wa.local:host")
+    {
+      "Botname = setting.botname"
+      "Ownerbot = setting.ownername"
+      "fake = setting.fakereply"
+     }
+     {
+      ("Msgbot = Halo ${title} saya matchbox assisten bot whatsapp anda ada yang bisa saya bantu?")
+      ("Msgreply = ${allchat}")
+     }
+     {
+      ("Msgbot = Berikut menu bot yah/n/n/n *@SPEED*/n/n/n *@JOKE*")
+      ("Msgreply = ${command}menu")
+
+//Jangan ubah kodenya

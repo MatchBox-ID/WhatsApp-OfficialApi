4c49b4b53ca1ebbe5f197d7527736d10a00b7e17
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-09-13 14:33:38 +0700

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,9 @@
+QRWHATSAPP V2.2.1
+QRWA.WEB V1.22.96
+WHAPI WEB V72.13.2O
+
+Qr ; maintanenc.opening/~local:host####wa.web
+{
+   wa.web/qr~\\\\script.only#####{sending{berhasil!
+   wa.web/qr~////script.only#####{sending{gagal!
+   wa.web/qr~\/\/script.terpasang#####scan.ulang

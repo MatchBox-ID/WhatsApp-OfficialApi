160d8b006cbb631c975379a0320c96bf608d9b2d
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-09-13 13:24:09 +0700

    Create token.js

---
+++ token.js
@@ -0,0 +1,8 @@
+{
+  "Token:AuchtOhq7T8619UH81jAu"
+  "Auth.token:MBOTV1BAH"
+  "Api.zeks:MBOTWHAPI8821"
+  "Ussername.Database:{database.js{auth.mysql"
+  "Password,Database:{password.js{auth.mysql"
+  "footer:WhatsApp Api Unofficial"
+}

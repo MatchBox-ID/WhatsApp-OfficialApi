2703bfc65e6b38a478a244fee9deee2265aeb309
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-09-14 16:19:58 +0700

    Create termux-server.js

---
+++ Meta/termux-server.js
@@ -0,0 +1,65 @@
+ if true /~npm start
+      echo;
+      ██╗░░██╗██████╗░
+      ╚██╗██╔╝██╔══██╗
+      ░╚███╔╝░██║░░██║
+      ░██╔██╗░██║░░██║
+      ██╔╝╚██╗██████╔╝
+      ╚═╝░░╚═╝╚═════╝░ 
+      WHATSAPP API BERHASIL MASUK KE PERANGKAT ANDA!
+ if false /~npm start
+      echo;
+      ██╗░░██╗██████╗░
+      ╚██╗██╔╝██╔══██╗
+      ░╚███╔╝░██║░░██║
+      ░██╔██╗░██║░░██║
+      ██╔╝╚██╗██████╔╝
+      ╚═╝░░╚═╝╚═════╝░ 
+      WHATSAPP API GAGAL MASUK KE PERANGKAT SERVER ANDA!
+}
+}
+    { if server/~ connect... #### /~ problem... Connecting...
+
+echo ;
+ WHATSAPP API 1%
+ WHATSAPP API 2%
+ WHATSAPP API 3%
+ WHATSAPP API 12%
+ WHATSAPP API 15%
+ WHATSAPP API 19%
+ WHATSAPP API 20%
+ WHATSAPP API 21%
+ WHATSAPP API 26%
+ WHATSAPP API 73%
+ WHATSAPP API 82%
+ WHATSAPP API 99%
+ WHATSAPP API 100%
+ WHATSAPP API 1%
+ WHATSAPP API 2%
+ WHATSAPP API 3%
+ WHATSAPP API 12%
+ WHATSAPP API 15%
+ WHATSAPP API 19%
+ WHATSAPP API 20%
+ WHATSAPP API 21%
+ WHATSAPP API 26%
+ WHATSAPP API 73%
+ WHATSAPP API 82%
+ WHATSAPP API 99%
+ WHATSAPP API 100%
+ WHATSAPP API 1%
+ WHATSAPP API 2%
+ WHATSAPP API 3%
+ WHATSAPP API 12%
+ WHATSAPP API 15%
+ WHATSAPP API 19%
+ WHATSAPP API 20%
+ WHATSAPP API 21%
+ WHATSAPP API 26%
+ WHATSAPP API 73%
+ WHATSAPP API 82%
+ WHATSAPP API 99%
+ WHATSAPP API 100%
+ Connection server..
+ Connection server..
+Berhasil...

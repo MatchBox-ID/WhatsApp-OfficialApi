872ffb4dc819784f955aeb3c3e1fd1ddfabff58c
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-09-14 16:13:41 +0700

    Create meta.js

---
+++ Meta/meta.js
@@ -0,0 +1,39 @@
+//file ini untuk perkembangan multi device dari meta
+
+{'Version : multi device'
+{'Version.perangkat : 14, 12, 13'
+{'WABetaInfo : www.WAbetainfo.com'
+}
+{Qr : qr{compatible/~scan.qr////ready~##script
+ : if true {echo;berhasil masuk server!
+ : if false {echo;gagal masuk ke server!
+
+ ; if true /~npm start
+     echo; 
+      ██╗░░██╗██████╗░
+      ╚██╗██╔╝██╔══██╗
+      ░╚███╔╝░██║░░██║
+      ░██╔██╗░██║░░██║
+      ██╔╝╚██╗██████╔╝
+      ╚═╝░░╚═╝╚═════╝░ 
+      WHATSAPP CONNECT! API SERVER TELAH BERHASIL TERSAMBUNG KE PERANGKAT ANDA!
+ ; if false /~npm start
+     echo;
+      ██╗░░██╗██████╗░
+      ╚██╗██╔╝██╔══██╗
+      ░╚███╔╝░██║░░██║
+      ░██╔██╗░██║░░██║
+      ██╔╝╚██╗██████╔╝
+      ╚═╝░░╚═╝╚═════╝░ 
+      WHATSAPP UNCONNECT! API SERVER GAGAL MASUK KE PERANGKAT ANDA SILAHKAN KETIK NPM START LAGI!
+ ; if echo;
+      ██╗░░██╗██████╗░
+      ╚██╗██╔╝██╔══██╗
+      ░╚███╔╝░██║░░██║
+      ░██╔██╗░██║░░██║
+      ██╔╝╚██╗██████╔╝
+      ╚═╝░░╚═╝╚═════╝░ 
+      POWERED BY MATCHBOX API TEAM
+
+/Jangan ubah apapun juga yah njing!
+

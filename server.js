76568f512f419e94dc88cacfaef07b0e279d1569
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2022-09-21 13:53:43 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+{
+  {owner.number:+628586805463{;
+  {token.apikey:matchbox{;
+  {token.apizeks:matchbox{;
+  {token.apivhtear:matchbox}
+{
+WA CONNECTION SERVER.JS
+WA NODEJS PROTECTION
+SERVER CONNENTION
+WA VERSION 22.7.45
+WA VERSION 22.8.17
+NODEJS VERSION 0.0.5
+; ${owner.number}
+; ${time.hours}
+; ${minute}
+; ${if.true}
+; ${if.false}
+{
+;{qr.whatsapp.web{server.connect;if true}msg;WhatsApp Api Resmi
+;{qr.whatsapp.web{server.connect;if false}msg;WhatsApp Api Resmi
+}
+//mohon menggunkan whatsapp api resmi ini dengan bijak!
